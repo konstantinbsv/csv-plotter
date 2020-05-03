88017d263f0412062b227e19fa9a11505e8d1437
Konstantin Borissov <32213296+konstantinbsv@users.noreply.github.com>
2020-05-03 14:35:40 -0700

    Implement pyplot

--- excel-plotter.py
+++ excel-plotter.py
@@ -2,7 +2,7 @@ import csv
 import os.path
 import sys
 import re
-import matplotlib.pyplot
+import matplotlib.pyplot as plt
 
 # regex data format definition
 RE_DATA = r'(\(\d{4}\))(\d.\d+)'
@@ -11,6 +11,8 @@ RAW_DATA_FILE = 'data.txt'
 CSV_DATA_FILE = 'csv_data.csv'
 FIELDNAMES = ['=\"(0000)\"', '=\"(0001)\"', '=\"(0010)\"', '=\"(0011)\"', '=\"(0100)\"', '=\"(0101)\"',
               '=\"(0110)\"', '=\"(0111)\"', '=\"(1000)\"', '=\"(1001)\"', '=\"(1010)\"', '=\"(1011)\"', '=\"(1100)\"']
+# plot definitions
+PLOT_SIZE = (10, 6)     # in hundreds of pixels
 
 # check if data.txt exists
 if not os.path.exists(RAW_DATA_FILE):
@@ -18,7 +20,7 @@ if not os.path.exists(RAW_DATA_FILE):
     sys.exit()
 
 with open(RAW_DATA_FILE, 'r') as raw_data:
-    clean = [line.strip() for line in raw_data]                     # remove extraneous characters
+    clean = [line.strip() for line in raw_data]                       # remove extraneous characters
     # lines = [line.split(')') for line in clean if line]             # split at ')' and remove empty lines
 
     lines = []
@@ -37,17 +39,27 @@ with open(RAW_DATA_FILE, 'r') as raw_data:
         num_sensors = num_sensors + 1
 
     num_rows = int(len(data_dict) / num_sensors)    # calculate number of rows in CSV
-    combined_dict = [{}] * num_rows                 # create list of dictionaries (one dict for each row)
+    list_of_dicts = [{}] * num_rows                 # create list of dictionaries (one dict for each row)
 
     # combine dictionaries in list for each row
     for row in range(num_rows):
         for i in range(num_sensors):
-            combined_dict[row] = {**combined_dict[row], **data_dict[row * num_sensors + i]}
+            list_of_dicts[row] = {**list_of_dicts[row], **data_dict[row * num_sensors + i]}
 
     # create CSV (overwrite if it already exists)
-    with open(CSV_DATA_FILE, 'w') as csv_file:
+    with open(CSV_DATA_FILE, 'w', newline='') as csv_file:
         csv_writer = csv.DictWriter(csv_file, fieldnames=FIELDNAMES, delimiter=',')
         csv_writer.writeheader()
-        csv_writer.writerows(combined_dict)
+        csv_writer.writerows(list_of_dicts)
 
+
+for field in FIELDNAMES:
+    sens = [float(line.get(field, 0)) for line in list_of_dicts if line]
+    title = field.strip('="')
+    fig = plt.figure(figsize=PLOT_SIZE)
+    plt.plot(sens)
+    plt.title(title)
+    plt.ylabel('picoFarads')
+    plt.xlabel('Sample number')
+    plt.savefig(title)
 
